7a5b441b57ab77874027bdbbe31583c4fd77472f
Elijahki <elijahkiprotich18427143@gmail.com>
2025-09-23 19:01:45 +0100

    Add files via upload

---
+++ My units program .c
@@ -0,0 +1,21 @@
+//simple semester units program
+/*
+Name :Kiprotich Ng'etich
+Reg :PA106/G/28708/25
+Description :simple program to display SE semester one , year one units
+*/
+
+#include<stdio.h>//precessor directive ;scanf(""), printf(),
+//main function
+int main () {
+printf    ("Simple program to display SE semester one, first year\n ");
+printf("1.Discrete Mathematics \n") ;
+printf("2.Mathematics for science \n") ;
+printf ("3.Fundermentals of ICT \n") ;
+printf ("4. Computer system and organization \n" ) ;
+printf ("5. Communication skills \n") ;
+printf("6. Introduction to programming and problem solving \n");
+printf("7. Fundermentals of system analysis and design ");
+
+return 0;
+}
