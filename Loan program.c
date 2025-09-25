78d7fc5da8de00f4cb61e54f0fcdfa122810ceec
Elijahki <elijahkiprotich18427143@gmail.com>
2025-09-25 07:47:56 +0300

    Update Loan program.c

--- Loan program.c
+++ Loan program.c
@@ -1,25 +1,29 @@
 //Bank customer program
 /*
-Name:
-Reg:
-Description:
+Name:Kiprotich Ngetich
+Reg:PA106/G/28708/25
+Description:Program to decide wheather qualify for loan or not
 */
 
 #include<stdio.h>
 int main(){
 	
 	float Age,Annual_income;
-	
+
+	//prompt the user to enter age
 	printf("Enter Age:");
 	scanf("%f",&Age);
-	
+
+	//prompt the user to enter annual income
 	printf("Enter annual income:");
 	scanf("%f",&Annual_income);
-	
+
+	//Display wheather one qualifies for loan or not
 	if(Age>=21&&Annual_income>=21000){
 	printf("Congratulations,you qualify for loan.");
 	}else{
 	printf("Unfortunately,we are unable to offer you loan at this time.");
 }
 return 0;
+
 }
